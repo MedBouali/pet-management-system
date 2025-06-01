dc5a4dfb5b49c6eb12eae50deaf8516c7f3e4112
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-06-01 10:04:45 +0100

    Add basic Spring Boot setup

---
+++ src/test/java/com/med/pet_management_system/PetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.pet_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
